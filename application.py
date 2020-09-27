388b872a6d589ade37abb4c072f476a22902ed23
Vishnu Raveendran <vishnunilambur@gmail.com>
2020-09-27 21:39:37 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,50 @@
+import torch
+from torchvision.transforms import ToTensor
+from flask import Flask, render_template, url_for, request, redirect, session
+from PIL import Image
+from io import BytesIO
+import base64
+
+from file_ops import Net
+
+application = Flask(__name__)
+net = Net()
+net.load_state_dict(torch.load("./model.h5"))
+to_tensor = ToTensor()
+application.config["SECRET_KEY"] = "my_secret_key"
+
+
+@application.route("/")
+def home():
+    result = None
+    if "result" in session:
+        result = {
+            "class": session["result"],
+            "image_string": session["image_string"]
+        }
+        del session["result"]
+        del session["image_string"]
+    return render_template("main.html", result=result)
+
+
+@application.route("/upload", methods=["POST"])
+def upload_image():
+    data = request.files["image"]
+    bytes_io = BytesIO(data.stream.read())
+    image = Image.open(bytes_io)
+    image_tensor = to_tensor(image)
+    assert image_tensor.size() == (1, 28, 28)
+    image_tensor = torch.reshape(image_tensor, (1, 1, 28, 28))
+    output = net(image_tensor)
+    session["result"] = torch.argmax(output).item()
+    session["image_string"] = base64.b64encode(bytes_io.getvalue()).decode('utf-8')
+    return redirect(url_for('home'))
+
+
+@application.errorhandler(500)
+def server_error(e):
+    return render_template("error.html"), 500
+
+
+if __name__ == '__main__':
+    application.run()
---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    MONGO_URL = "mongodb+srv://dbuser_admin_p0:68C%40Dz%40545a5@website-cluster.kqodo.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    MAX_POOL_SIZE = 10
+    MIN_POOL_SIZE = 1
